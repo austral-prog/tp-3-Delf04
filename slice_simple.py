fe0c4713748695c62e4f1bfaa58e6662a027625f
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-10 16:35:01 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -6,3 +6,13 @@ def slice_simple():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+
+        texto = input("Colocar palabra: \n").lower()
+        primeras_3 = texto[:3]
+        las_del_medio = texto[3:6]
+        las4primera = texto[0:4] 
+        laantepenultima = texto[-3:]
+        lasultimas_letras = las4primera + laantepenultima
+        print(primeras_3)
+        print(las_del_medio)
+        print(lasultimas_letras)
