8aef092a56f0af0abba80d6522c290178423a96f
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-10 16:44:03 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -4,12 +4,3 @@ def slice_advanced():
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
 
-        texto = input("Colocar palabra: \n").lower()
-        primeras_3 = texto[:3]
-        las_del_medio = texto[3:6]
-        las4primera = texto[0:4] 
-        laantepenultima = texto[-3:]
-        lasultimas_letras = las4primera + laantepenultima
-        print(primeras_3)
-        print(las_del_medio)
-        print(lasultimas_letras)
