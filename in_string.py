5c2730da19c571b84b6317be9d54c09cb966bf6b
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-10 15:24:58 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -4,3 +4,18 @@ def check_vowels():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+
+    nombre = input("Poner Nombre \n>")
+    nombre_minus = nombre.lower()
+    
+    vocal_a = "a"  in nombre_minus
+    vocal_e = "e"  in nombre_minus
+    vocal_i = "i"  in nombre_minus
+    vocal_o = "o"  in nombre_minus
+    vocal_u = "u"  in nombre_minus
+
+    print(vocal_a)
+    print(vocal_e)
+    print(vocal_i)
+    print(vocal_o)
+    print(vocal_u)
