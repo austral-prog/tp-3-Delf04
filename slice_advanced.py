fbec12191f98e691302a4e98ce7ff8ab911b6c69
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-10 16:54:48 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,4 +3,4 @@ def slice_advanced():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
-
+    texto = input("Poner palabra \n")
