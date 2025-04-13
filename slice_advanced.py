e935047611f4588377d929317f185a8308c5563b
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-12 23:52:39 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,8 +1,8 @@
 def slice_advanced():
     # Código a implementar utilizando input.
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    # Para verificar este ejercicio ejecutar el comando
+    # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
     texto = input("Poner palabra \n")
     primersalto = texto[4::2]
-    print (primersalto)
+    print(primersalto)
